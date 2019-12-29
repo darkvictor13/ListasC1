c3082338b7b07eba54e52d30d018bbd077db4e13
victor-almeida <sistema-safe@localhost.localdomain>
2019-12-28 22:00:52 -0300

    esqueci do ultimo

--- (2-bim)lista-for/12.c
+++ (2-bim)lista-for/12.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
-void printa_linha (int n) {
+void printaChar (int n) {
   char i, j;
   for (i = j = 0; i < n; i++, j++) {
     printf("%c", 'a' + j);
   }
 }
 
-void printa_espaco (int n) {
+void printaEspaco (int n) {
   for (int i = 1; i <= n; i++) {
     printf("%c", ' ');
   }
 }
 
 int main () {
-  int x,count_p,count_n;
-  scanf("%d",&x);
+  int num_linhas, count_p, count_n;
+  scanf("%d",&num_linhas);
   count_p = 1;
-  count_n = x-1;
-  for (int i = 1;i <= x;count_p++,count_n--,i++){
-    printa_espaco (count_n);
-    printa_linha (count_p);
+  count_n = num_linhas - 1;
+  for (int i = 1; i <= num_linhas; count_n--, count_p++, i++){
+    printaEspaco(count_n);
+    printaChar(count_p);
     printf("\n");
   }
   return 0;
