9c05c802ff67d3c5ae4cdc28325cedd71addc628
Victor Emanuel Almeida <51101723+darkvictor13@users.noreply.github.com>
2020-01-02 01:09:56 -0300

    aaaaaaa

--- (2-bim)lista-vetor/05.c
+++ (2-bim)lista-vetor/05.c
@@ -9,10 +9,18 @@ void lerVet(int v[], int t) {
   }
 }
 
+void lerVetPonteiro(int *v, int t) {
+  int *fim = (v + t);
+  for (; v < fim; v++) {
+    scanf("%d", v);
+  }
+}
+
 int anteriorEProximoPares(int v[], int t) {
   int i, n = 0;
   for (i = 1; i < (t - 1); i++) {
     if (!(v[i-1] % 2) && !(v[i+1] % 2)) {
+    //if (((v[i-1] % 2) == 0) && ((v[i+1] % 2) == 0))
     //se o anterior (e) o sucessor divididos por 2 dão zero
       n++;
     }
