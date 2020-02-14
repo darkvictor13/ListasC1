064ddd8b0bcd58851fa31f8902aaeff925d6e4fc
darkvictor13 <victoralmeida2001@hotmail.com>
2020-02-14 17:02:25 -0300

    lista string feita

---
+++ (3-bim)2-lista-strings/[ponteiro]->01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void preencheStr (char str[],char x,int t) {
+  char *p = (str + t);
+
+  for (; str < p; str++) {
+    *str = x;
+  }
+}
+
+int main () {
+  char str[51],x;
+  int tam,casos;
+
+  for (scanf("%d",&casos); casos > 0; casos--) {
+    scanf ("%d %c",&tam,&x);
+    preencheStr(str,x,tam);
+    printf("[%s]\n");
+  }
+  return 0;
+}
---
+++ (3-bim)2-lista-strings/[ponteiro]->03.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void fazPadrao (char *str,char x,int t) {
+  int i,aux;
+
+  for (i = 0; i < t; i++) {
+    for (aux = 0; aux <= i; aux++,str++) {
+      *str = x;
+    }
+    *str = ' ';
+    str++;
+  }
+  str--;
+  *str = 0;
+}
+
+int main () {
+  char str[50],c;
+  int t,casos;
+
+  for (scanf("%d",&casos); casos > 0; casos--) {
+    scanf("%d %c",&t,&c);
+    fazPadrao(str,c,t);
+    printf("[%s]\n",str);
+  }
+  return 0;
+}
